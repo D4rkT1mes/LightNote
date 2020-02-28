a7eebaabc1d347e783847611d76956a19b305d9a
StacyCookie <22609343+StacyCookie@users.noreply.github.com>
2020-02-28 22:44:29 +0300

    bruh

---
+++ app/src/main/java/com/nanasdev/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.nanasdev.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goTo(View view) {
+        Intent i = new Intent(this, Note.class);
+        startActivity(i);
+    }
+}
