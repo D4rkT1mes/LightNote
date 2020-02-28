c4487893128cd557747b748f20aa8aa4034c178b
StacyCookie <anastasia.romashka@gmail.com>
2020-02-29 00:14:48 +0300

    bruhV2

--- app/src/main/java/com/nanasdev/myapplication/Note.java
+++ app/src/main/java/com/nanasdev/myapplication/Note.java
@@ -1,15 +1,20 @@
 package com.nanasdev.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Note extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.note_view
-        );
+        setContentView(R.layout.activity_note);
     }
 
+    public void goToMAIN(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
 
 }
