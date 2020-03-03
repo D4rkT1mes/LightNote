e9d4ae548fd575dc76a2905dc46411a9164e79ca
StacyCookie <anastasia.romashka@gmail.com>
2020-03-03 19:44:30 +0300

    bruh_v2

--- app/src/main/java/com/nanasdev/myapplication/Note.java
+++ app/src/main/java/com/nanasdev/myapplication/Note.java
@@ -2,7 +2,12 @@ package com.nanasdev.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +17,18 @@ public class Note extends AppCompatActivity {
         setContentView(R.layout.activity_note);
     }
 
+    public void exitNote(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.exit_note_popup, popup.getMenu());
+        popup.show();
+    }
+
     public void goToMAIN(View view) {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
+
+
 
 }
