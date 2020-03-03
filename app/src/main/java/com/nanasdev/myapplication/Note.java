541eef47dd506e5004a9367178c8810d67752b92
StacyCookie <anastasia.romashka@gmail.com>
2020-03-03 21:07:25 +0300

    bruh_v2

--- app/src/main/java/com/nanasdev/myapplication/Note.java
+++ app/src/main/java/com/nanasdev/myapplication/Note.java
@@ -11,23 +11,24 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Note extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note);
-    }
+    public class Note extends AppCompatActivity {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_note);
+        }
 
-    public void exitNote(View v) {
-        PopupMenu popup = new PopupMenu(this, v);
-        MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.exit_note_popup, popup.getMenu());
-        popup.show();
-    }
+        public void exitNote(View v) {
+            PopupMenu popup = new PopupMenu(this, v);
+            MenuInflater inflater = popup.getMenuInflater();
+            inflater.inflate(R.menu.exit_note_popup, popup.getMenu());
+            popup.show();
 
-    public void goToMAIN(View view) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-    }
+        }
+
+        public void goToMAIN(View view) {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
 
 
 
