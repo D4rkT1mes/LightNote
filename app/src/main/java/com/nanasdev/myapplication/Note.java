1891fee63c70ba5e3b291fac7404dfb843c34cb5
D4rkT1mes <anastasia.romashka@gmail.com>
2020-03-08 17:08:40 +0300

    bruh_v2.4

--- app/src/main/java/com/nanasdev/myapplication/Note.java
+++ app/src/main/java/com/nanasdev/myapplication/Note.java
@@ -27,7 +27,8 @@ public class Note extends AppCompatActivity implements PopupMenu.OnMenuItemClick
 
         //define buttons from note_header.xml lol
         headline = findViewById(R.id.headline);
-        maintext = findViewById(R.id.maintext);
+        maintext = findViewById(R.id.maintex);
+        //asdkadsapf
     }
 
 
